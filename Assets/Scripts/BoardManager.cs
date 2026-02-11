d60dcdabaa461f7fb660090d6a5ca3075338d8ac
minh897 <minhnhn897@gmail.com>
2026-02-11 15:22:13 +0700

    Added board data

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public class CellData
+{
+    public bool passable;
+}
+
 public class BoardManager : MonoBehaviour
 {
     [SerializeField] private int tileWidth;
@@ -9,6 +14,7 @@ public class BoardManager : MonoBehaviour
     [SerializeField] private Tile[] wallTiles;
 
     private Tilemap _tilemap;
+    private CellData[,] _boardData;
 
     void Awake()
     {
@@ -17,19 +23,24 @@ public class BoardManager : MonoBehaviour
 
     void Start()
     {
+        _boardData = new CellData[tileWidth, tileHeight];
+
         for (int y = 0; y < tileHeight; y++)
         {
             for (int x = 0; x < tileWidth; x++)
             {
                 Tile tile;
+                _boardData[x, y] = new();
 
                 if (x == 0 || y == 0 || x == tileWidth - 1 || y == tileHeight - 1)
                 {
                     tile = wallTiles[Random.Range(0, wallTiles.Length)];
+                    _boardData[x, y].passable = false;
                 }
                 else
                 {
                     tile = groundTiles[Random.Range(0, groundTiles.Length)];
+                    _boardData[x, y].passable = true;
                 }
 
                 Vector3Int position = new(x, y, 0);
