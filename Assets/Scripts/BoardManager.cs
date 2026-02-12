3c9ccdfb666b623e6968a6504fd21fb5db837654
minh897 <minhnhn897@gmail.com>
2026-02-12 15:17:03 +0700

    Added moving the player

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -61,4 +61,15 @@ public class BoardManager : MonoBehaviour
     {
         return _grid.GetCellCenterWorld((Vector3Int)cellIndex);
     }
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= tileWidth
+            || cellIndex.y < 0 || cellIndex.y >= tileHeight)
+        {
+            return null;
+        }
+
+        return _boardData[cellIndex.x, cellIndex.y];
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,79 @@
+using InputActions;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private BoardManager _board;
     private Vector2Int _cellPosition;
+
+    private PlayerInputActions inputActions;
+    private Vector2Int direction;
+
+    private bool hasMoved = false;
+
+    void Awake()
+    {
+        inputActions = new();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Move.started += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+
+        inputActions.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Move.started -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+
+        inputActions.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            var input = context.ReadValue<Vector2>();
+            direction = new((int)input.x, (int)input.y);
+        }
+
+        if (context.canceled)
+        {
+            direction = Vector2Int.zero;
+        }
+    }
+
+    void Update()
+    {
+        Vector2Int newCellTarget = _cellPosition;
+
+        if (direction.sqrMagnitude != 0 && !hasMoved)
+        {
+            hasMoved = true;
+            newCellTarget += direction;
+        }
+
+        if (direction.sqrMagnitude == 0)
+        {
+            hasMoved = false;
+        }
+
+        // check for a passable tile
+        // then move there if it is
+        if (hasMoved)
+        {
+            CellData cellData = _board.GetCellData(newCellTarget);
+            if (cellData != null && cellData.passable)
+            {
+                _cellPosition = newCellTarget;
+                transform.position = _board.CellToWorld(_cellPosition);
+            }
+        }
+    }
 
     public void Spawn(BoardManager boardManager, Vector2Int cell)
     {
