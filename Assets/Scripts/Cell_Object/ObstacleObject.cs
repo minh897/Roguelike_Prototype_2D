918cfd0e893119090e67cf767a6565c626a8b50c
minh897 <minhnhn897@gmail.com>
2026-02-27 15:10:44 +0700

    Test changing obstacle tile via SpriteRenderer

--- Assets/Scripts/Cell_Object/ObstacleObject.cs
+++ Assets/Scripts/Cell_Object/ObstacleObject.cs
@@ -1,25 +1,28 @@
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class ObstacleObject : CellObject
 {
-    public int maxHealth;
-    public Tile obstacleTile;
-    public Tile damagedTile;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private Tile obstacleTile;
+    [SerializeField] private Tile damagedTile;
 
     private int _healthPoint;
     private Tile _originalTile;
+    private SpriteRenderer sRenderer;
   
     public override void Init(Vector2Int cell)
     {
         base.Init(cell);
 
         _healthPoint = maxHealth;
+        sRenderer = GetComponent<SpriteRenderer>();
 
         // cache the ground tile from the board as orignal tile 
         // before setting the current cell with an obstacle tile
         _originalTile = GameManager.Instance.GetBoard().GetCellTile(cell);
-        GameManager.Instance.GetBoard().SetCellTile(cell, obstacleTile);
+        // GameManager.Instance.GetBoard().SetCellTile(cell, obstacleTile);
     }
 
     public override bool PlayerWantsToEnter()
@@ -30,7 +33,8 @@ public class ObstacleObject : CellObject
         {
             if (_healthPoint == 1)
             {
-                GameManager.Instance.GetBoard().SetCellTile(_cell, damagedTile);
+                // Replace the sprite with the damagedTile sprite
+                sRenderer.sprite = damagedTile.sprite;
             }
             return false;
         }
