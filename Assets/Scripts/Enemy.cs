cb92cc8ff3adee9b4d6b1bd290d63ad144ae7b2b
minh897 <minhnhn897@gmail.com>
2026-03-04 15:26:51 +0700

    Refactor some code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -49,8 +49,9 @@ public class Enemy : CellObject
         var absYDist = Mathf.Abs(yDist);
 
         // Enemy stop moving and attack when close to the player
-        if (absXDist == 1 && yDist == 0
-             || absYDist == 1 && xDist == 0)
+        bool adjacentHorizontally = absXDist == 1 && yDist == 0;
+        bool adjacentVertically = absYDist == 1 && xDist == 0;
+        if (adjacentHorizontally || adjacentVertically)
         {
             GameManager.Instance.ChangeFood(-2);
             return;
@@ -58,20 +59,20 @@ public class Enemy : CellObject
 
         if (absYDist >= absXDist)
         {
-            MoveInDirection(yDist, Vector2Int.up);
+            MoveTowardAxis(yDist, Vector2Int.up);
         }
         else
         {
-            MoveInDirection(xDist, Vector2Int.right);
+            MoveTowardAxis(xDist, Vector2Int.right);
         }
     }
 
-    private void MoveInDirection(int dist, Vector2Int direction)
+    private void MoveTowardAxis(int dist, Vector2Int positiveDirection)
     {
         // Pick a direction base on the distance from the player
         // if it's positive then move along the original direction
         // else move along the opposite
-        var moveDir = dist > 0 ? direction : -direction;
+        var moveDir = dist > 0 ? positiveDirection : -positiveDirection;
         TryMoveTo(_cell + moveDir);
     }
 
@@ -80,8 +81,8 @@ public class Enemy : CellObject
         var board = GameManager.Instance.GetBoard();
         var targetCell = board.GetCellData(coord);
 
-        if (!targetCell.passable || 
-            targetCell == null || 
+        if (targetCell == null || 
+            !targetCell.passable || 
             targetCell.containedObject != null)
         {
             return;
